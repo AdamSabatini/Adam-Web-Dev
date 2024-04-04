4b4a3ad536c838ace57b85b3f5652ffa45575057
AdamSabatini <Adamsabatini13@gmail.com>
2024-04-03 20:17:15 -0400

    implemented randomValueFromArray()

--- Assignment-4/Silly-Story-Generator/main.js
+++ Assignment-4/Silly-Story-Generator/main.js
@@ -11,3 +11,29 @@ const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. W
 const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas']
 const insertY = ['the soup kitchen', 'Disneyland', 'the White House']
 const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away']
+
+randomize.addEventListener('click', result);
+
+function result() {
+  let newStory = storyText
+  
+  const xItem = randomValueFromArray(insertX)
+  const yItem = randomValueFromArray(insertY)
+  const zItem = randomValueFromArray(insertZ)
+  
+  
+
+  if(customName.value !== '') {
+    const name = customName.value;
+
+  }
+
+  if(document.getElementById("uk").checked) {
+    const weight = Math.round(300);
+    const temperature =  Math.round(94);
+
+  }
+
+  story.textContent = ;
+  story.style.visibility = 'visible';
+}
