454951972cd930e76c17348cc21d06e4b15c3188
AdamSabatini <Adamsabatini13@gmail.com>
2024-04-04 19:32:23 -0400

    update collisionDetect method

--- Assignment-4/Adding-Features-to-Bouncing-Balls-Demo/main-finished.js
+++ Assignment-4/Adding-Features-to-Bouncing-Balls-Demo/main-finished.js
@@ -74,17 +74,17 @@ class Ball extends Shape{
 
   collisionDetect() {
     for (const ball of balls) {
-      if (!(this === ball)) {
+      if (!(this === ball) && ball.exists) {
         const dx = this.x - ball.x;
         const dy = this.y - ball.y;
         const distance = Math.sqrt(dx * dx + dy * dy);
-
+  
         if (distance < this.size + ball.size) {
           ball.color = this.color = randomRGB();
         }
       }
     }
-  }
+  }  
 }
 
 const balls = [];
