31de0a10eeffa37ada3d64106c72f6006fdfd466
AdamSabatini <Adamsabatini13@gmail.com>
2024-04-03 21:08:26 -0400

    image array and object with alt text for images

--- Assignment-4/Image-Gallery/main.js
+++ Assignment-4/Image-Gallery/main.js
@@ -5,8 +5,16 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const imageArray = ['pic1.jpg','pic2.jpg','pic3.jpg','pic4.jpg','pic5.jpg']
 
 /* Declaring the alternative text for each image file */
+const altText = {
+  'pic1.jpg' : 'Closeup of a human eye',
+  'pic2.jpg' : 'Rock that has a wave-like pattern',
+  'pic3.jpg' : 'white and purple flowers',
+  'pic4.jpg' : 'wall with egyptian hieroglyphs',
+  'pic5.jpg' : 'A moth on a leaf'
+}
 
 /* Looping through images */
 
