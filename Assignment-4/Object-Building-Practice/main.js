7ab6e5f5991002a181ff40132e9d191bfe3d391d
AdamSabatini <Adamsabatini13@gmail.com>
2024-04-04 17:57:46 -0400

    method to animate the balls

--- Assignment-4/Object-Building-Practice/main.js
+++ Assignment-4/Object-Building-Practice/main.js
@@ -43,12 +43,57 @@ class Ball {
     ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
     ctx.fill();
   }
+  // method to check if ball has hit the edge of the window
+  update() {
+    if ((this.x + this.size) >= width) {
+      this.velX = -(this.velX);
+    }
+  
+    if ((this.x - this.size) <= 0) {
+      this.velX = -(this.velX);
+    }
+  
+    if ((this.y + this.size) >= height) {
+      this.velY = -(this.velY);
+    }
+  
+    if ((this.y - this.size) <= 0) {
+      this.velY = -(this.velY);
+    }
+  
+    this.x += this.velX;
+    this.y += this.velY;
+  }
+  
 }
 
+// test ball
 const testBall = new Ball(50, 100, 4, 4, "blue", 10);
 testBall.x;
 testBall.size;
 testBall.color;
 testBall.draw();
+
+// animate the ball
+const balls = [];
+
+while (balls.length < 25) {
+  const size = random(10, 20);
+  const ball = new Ball(
+    // ball position always drawn at least one ball width
+    // away from the edge of the canvas, to avoid drawing errors
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-7, 7),
+    random(-7, 7),
+    randomRGB(),
+    size,
+  );
+
+  balls.push(ball);
+}
+
+
+
 
 
