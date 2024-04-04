9d43390301ae53067b80b6db5b7bb59ae8ba53ba
AdamSabatini <Adamsabatini13@gmail.com>
2024-04-03 20:09:17 -0400

    format change

--- Assignment-4/Silly-Story-Generator/main.js
+++ Assignment-4/Silly-Story-Generator/main.js
@@ -8,6 +8,6 @@ function randomValueFromArray(array){
 }
 
 const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day."
-const insertX = "Willy the Goblin Big Daddy Father Christmas"
-const insertY = "the soup kitchen Disneyland the White House"
-const insertZ = "spontaneously combusted melted into a puddle on the sidewalk turned into a slug and crawled away"
+const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas']
+const insertY = ['the soup kitchen', 'Disneyland', 'the White House']
+const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away']
