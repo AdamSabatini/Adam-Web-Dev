e122444e219319c9f9c108a20f11820c974cdf25
AdamSabatini <Adamsabatini13@gmail.com>
2024-04-03 20:31:03 -0400

    added uk functionality

--- Assignment-4/Silly-Story-Generator/main.js
+++ Assignment-4/Silly-Story-Generator/main.js
@@ -31,11 +31,12 @@ function result() {
   }
 
   if(document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
-
+    const weight = `${Math.round(300*0.0714286)} stone`;
+    const temperature =  `${Math.round((94-32) * 5 / 9)} centigrade`;
+    newStory = newStory.replaceAll('94 fahrenheit', temperature);
+    newStory = newStory.replaceAll('300 pounds', weight);
   }
 
-  story.textContent = ;
+  story.textContent = newStory;
   story.style.visibility = 'visible';
 }
