2ddd00e8535a9fe91569c250cc36dd5c9ed24a4a
AdamSabatini <Adamsabatini13@gmail.com>
2024-04-03 20:21:32 -0400

    added newStory variables and completed first if block

--- Assignment-4/Silly-Story-Generator/main.js
+++ Assignment-4/Silly-Story-Generator/main.js
@@ -21,11 +21,13 @@ function result() {
   const yItem = randomValueFromArray(insertY)
   const zItem = randomValueFromArray(insertZ)
   
-  
+  newStory = newStory.replaceAll(':insertx:',xItem)
+  newStory = newStory.replaceAll(':inserty:',yItem)
+  newStory = newStory.replaceAll(':insertz:',zItem)
 
   if(customName.value !== '') {
     const name = customName.value;
-
+    newStory = newStory.replaceAll('Bob', name)
   }
 
   if(document.getElementById("uk").checked) {
